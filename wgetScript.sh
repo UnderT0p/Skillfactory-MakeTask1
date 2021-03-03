a73486d4c99ac38efac8c038bdd979fcb0dbe67d
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-03-03 14:15:00 +0300

    Update wgetScript.sh

--- wgetScript.sh
+++ wgetScript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget --mirror -p --convert-links -P $* https://www.chiark.greenend.org.uk/~sgtatham/bugs.html
-mv $*/www.chiark.greenend.org.uk/~sgtatham/bugs.html /home/ansible/task1/www.chiark.greenend.org.uk/~sgtatham/index.html
+wget --mirror -p --convert-links -P /var/www $* https://www.chiark.greenend.org.uk/~sgtatham/bugs.html
+
