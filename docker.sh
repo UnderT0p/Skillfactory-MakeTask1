15ab1122809e82176c7006420616a251ea2b3dfd
UnderT0p <victordutovski@gmail.com>
2021-03-09 18:02:31 +0300

    neew

---
+++ docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo scp /var/www/www.chiark.greenend.org.uk/~sgtatham $1:/var/www/html
+sudo scp ~/nginxDoc.conf $1:~/nginxDoc.conf
+ssh $1 sudo docker run --name test3 -d -p 8080:80 -v ~/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
