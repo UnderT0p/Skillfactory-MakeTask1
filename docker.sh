3114577dd1bce4d216bb948645206abe90d526d0
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-03-09 17:44:06 +0300

    Delete docker.sh

--- docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo scp /var/www/www.chiark.greenend.org.uk/~sgtatham $1:/var/www/html
-sudo scp ~/nginxDoc.conf $1:~/nginxDoc.conf
-ssh $1 sudo docker run --name test3 -d -p 8080:80 -v ~/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
