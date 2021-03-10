a6bcadb390bebbc314d4aa5b04e8bf0157876e9d
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-03-10 17:19:28 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 sudo wget --mirror -p --convert-links -P /var/www https://www.chiark.greenend.org.uk/~sgtatham/bugs.html
-ssh $1 sudo rm -fr /var/www/html
-sudo scp /var/www/www.chiark.greenend.org.uk/~sgtatham $1:/var/www/html
+ssh $1 sudo rm -fr ~/htmlForBugs
+sudo scp /var/www/www.chiark.greenend.org.uk/~sgtatham $1:~/htmlForBugs
 ssh $1 sudo docker stop test3
 ssh $1 sudo docker rm test3
-ssh $1 sudo docker run --name test3 -d -p 8080:80 -v /var/www/html://usr/share/nginx/html:ro -v ~/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
+ssh $1 sudo docker run --name test3 -d -p 8080:80 -v ~/htmlForBugs://usr/share/nginx/html:ro -v ~/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
