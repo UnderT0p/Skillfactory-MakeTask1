c809defb678ed0bca16252693ee444bab36b02ef
UnderT0p <victordutovski@gmail.com>
2021-03-10 15:38:53 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo scp -r /var/www/www.chiark.greenend.org.uk/~sgtatham $1:~/
+sudo scp ~/Skillfactory-MakeTask1/nginxImage.tar $1:~/
+sudo scp /var/www/www.chiark.greenend.org.uk/~sgtatham $1:/var/www/html
 sudo scp ~/nginxDoc.conf $1:~/nginxDoc.conf
+ssh $1 sudo docker load -i ~/nginxImage.tar
 ssh $1 sudo docker run --name test3 -d -p 8080:80 -v ~/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
