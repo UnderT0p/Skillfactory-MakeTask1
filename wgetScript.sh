2754a8ce4d1ebee7d01abbef7a1d36fdd6198ced
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-03-10 17:11:22 +0300

    Update wgetScript.sh

--- wgetScript.sh
+++ wgetScript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo wget --mirror -p --convert-links -P /var/www https://www.chiark.greenend.org.uk/~sgtatham/bugs.html
+sudo wget --mirror -p --convert-links -P ~/ https://www.chiark.greenend.org.uk/~sgtatham/bugs.html
 
