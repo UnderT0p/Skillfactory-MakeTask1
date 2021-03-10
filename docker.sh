cdc7bb7fe61002ef7e46d58520e0e43c737376fe
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-03-10 18:00:01 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,6 +4,6 @@ sudo docker save -o ~/nginxImage.tar nginx
 ssh $1 mkdir ~/htmlForBugs
 sudo scp ~/nginxImage.tar $1:~/
 sudo scp -r ~/www.chiark.greenend.org.uk/~sgtatham $1:~/htmlForBugs/
-sudo scp ~/nginxDoc.conf $1:~/nginxDoc.conf
+sudo scp ~/Skillfactory-MakeTask1/nginxDoc.conf $1:~/nginxDoc.conf
 ssh $1 sudo docker load -i ~/nginxImage.tar
 ssh $1 sudo docker run --name test3 -d -p 8080:80 -v ~/htmlForBugs://usr/share/nginx/html:ro -v ~/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
