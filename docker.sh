d09d3f1e95ab660324c1b7dec8c9c12f9e569abf
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-03-10 17:03:45 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -5,4 +5,4 @@ sudo scp ~/nginxImage.tar $1:~/
 sudo scp /var/www/www.chiark.greenend.org.uk/~sgtatham $1:/var/www/html
 sudo scp ~/nginxDoc.conf $1:~/nginxDoc.conf
 ssh $1 sudo docker load -i ~/nginxImage.tar
-ssh $1 sudo docker run --name test3 -d -p 8080:80 -v ~/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
+ssh $1 sudo docker run --name test3 -d -p 8080:80 -v /var/www/html://usr/share/nginx/html:ro -v ~/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
