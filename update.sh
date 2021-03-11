bbe75cd15f45f2adef0e927e2e01176e2a44f60c
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-03-11 13:47:58 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@ ssh $user sudo rm ~/Skillfactory-MakeTask1/htmlForBugs/*
 sudo scp ~/Skillfactory-MakeTask1/www.chiark.greenend.org.uk/~sgtatham $user:~/Skillfactory-MakeTask1/htmlForBugs
 ssh $user sudo docker stop test3
 ssh $user sudo docker rm test3
-ssh $user sudo docker run --name test3 -d -p 8080:80 -v ~/Skillfactory-MakeTask1/htmlForBugs://usr/share/nginx/html:ro -v ~/Skillfactory-MakeTask1/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
+ssh $user sudo docker run --name test3 -d -p 8080:80 -v ~/Skillfactory-MakeTask1/htmlForBugs://usr/share/nginx/html:ro -v ~/Skillfactory-MakeTask1/nginx.conf://etc/nginx/nginx.conf:ro nginx
