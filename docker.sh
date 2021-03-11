f387bd25838f0aeb84f16633c7d0f3a893fac891
UnderT0p <victordutovski@gmail.com>
2021-03-11 11:51:19 +0300

    usernameAndmachine

--- docker.sh
+++ docker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+source ~/Skillfactory-MakeTask1/usernameAndmachine
 sudo docker pull nginx
 sudo docker save -o ~/Skillfactory-MakeTask1/nginxImage.tar nginx
-ssh $1 mkdir ~/Skillfactory-MakeTask1
-ssh $1 mkdir ~/Skillfactory-MakeTask1/htmlForBugs
-sudo scp ~/Skillfactory-MakeTask1/nginxImage.tar $1:~/Skillfactory-MakeTask1
-sudo scp -r ~/Skillfactory-MakeTask1/www.chiark.greenend.org.uk/~sgtatham $1:~/Skillfactory-MakeTask1/htmlForBugs
-sudo scp ~/Skillfactory-MakeTask1/nginxDoc.conf $1:~/Skillfactory-MakeTask1/nginxDoc.conf
-ssh $1 sudo docker load -i ~/Skillfactory-MakeTask1/nginxImage.tar
-ssh $1 sudo docker run --name test3 -d -p 8080:80 -v ~/Skillfactory-MakeTask1/htmlForBugs://usr/share/nginx/html:ro -v ~/Skillfactory-MakeTask1/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
+ssh $user mkdir ~/Skillfactory-MakeTask1
+ssh $user mkdir ~/Skillfactory-MakeTask1/htmlForBugs
+sudo scp ~/Skillfactory-MakeTask1/nginxImage.tar $user:~/Skillfactory-MakeTask1
+sudo scp -r ~/Skillfactory-MakeTask1/www.chiark.greenend.org.uk/~sgtatham $user:~/Skillfactory-MakeTask1/htmlForBugs
+sudo scp ~/Skillfactory-MakeTask1/nginxDoc.conf $user:~/Skillfactory-MakeTask1/nginxDoc.conf
+ssh $user sudo docker load -i ~/Skillfactory-MakeTask1/nginxImage.tar
+ssh $user sudo docker run --name test3 -d -p 8080:80 -v ~/Skillfactory-MakeTask1/htmlForBugs://usr/share/nginx/html:ro -v ~/Skillfactory-MakeTask1/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+source ~/Skillfactory-MakeTask1/usernameAndmachine
 sudo wget --mirror -p --convert-links -P ~/Skillfactory-MakeTask1 https://www.chiark.greenend.org.uk/~sgtatham/bugs.html
-ssh $1 sudo rm ~/Skillfactory-MakeTask1/htmlForBugs/*
-sudo scp ~/Skillfactory-MakeTask1/www.chiark.greenend.org.uk/~sgtatham $1:~/Skillfactory-MakeTask1/htmlForBugs
-ssh $1 sudo docker stop test3
-ssh $1 sudo docker rm test3
-ssh $1 sudo docker run --name test3 -d -p 8080:80 -v ~/Skillfactory-MakeTask1/htmlForBugs://usr/share/nginx/html:ro -v ~/Skillfactory-MakeTask1/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
+ssh $user sudo rm ~/Skillfactory-MakeTask1/htmlForBugs/*
+sudo scp ~/Skillfactory-MakeTask1/www.chiark.greenend.org.uk/~sgtatham $user:~/Skillfactory-MakeTask1/htmlForBugs
+ssh $user sudo docker stop test3
+ssh $user sudo docker rm test3
+ssh $user sudo docker run --name test3 -d -p 8080:80 -v ~/Skillfactory-MakeTask1/htmlForBugs://usr/share/nginx/html:ro -v ~/Skillfactory-MakeTask1/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
