f04b542dadb9b84e2680803d8830d0c9edc51463
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-03-11 11:21:22 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
-sudo wget --mirror -p --convert-links -P /var/www https://www.chiark.greenend.org.uk/~sgtatham/bugs.html
-ssh $1 sudo rm -fr ~/htmlForBugs
-sudo scp /var/www/www.chiark.greenend.org.uk/~sgtatham $1:~/htmlForBugs
+sudo wget --mirror -p --convert-links -P ~/Skillfactory-MakeTask1 https://www.chiark.greenend.org.uk/~sgtatham/bugs.html
+ssh $1 sudo rm ~/Skillfactory-MakeTask1/htmlForBugs/*
+sudo scp ~/Skillfactory-MakeTask1/www.chiark.greenend.org.uk/~sgtatham $1:~/Skillfactory-MakeTask1/htmlForBugs
 ssh $1 sudo docker stop test3
 ssh $1 sudo docker rm test3
-ssh $1 sudo docker run --name test3 -d -p 8080:80 -v ~/htmlForBugs://usr/share/nginx/html:ro -v ~/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
+ssh $1 sudo docker run --name test3 -d -p 8080:80 -v ~/Skillfactory-MakeTask1/htmlForBugs://usr/share/nginx/html:ro -v ~/Skillfactory-MakeTask1/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
