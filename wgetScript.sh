e3a8470b9fb1b8d8d65732e269e30f2616c8349b
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-03-11 11:54:46 +0300

    Update wgetScript.sh

--- wgetScript.sh
+++ wgetScript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo wget --mirror -p --convert-links -P ~/Skillfactory-MakeTask1 https://www.chiark.greenend.org.uk/~sgtatham/bugs.html
+wget --mirror -p --convert-links -P ~/Skillfactory-MakeTask1 https://www.chiark.greenend.org.uk/~sgtatham/bugs.html
 
