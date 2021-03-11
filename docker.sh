e6ccffa03c4fe48dfe0b4cda1ae22ba181f21266
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-03-11 13:43:54 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,6 +6,6 @@ ssh $user mkdir ~/Skillfactory-MakeTask1
 ssh $user mkdir ~/Skillfactory-MakeTask1/htmlForBugs
 sudo scp ~/Skillfactory-MakeTask1/nginxImage.tar $user:~/Skillfactory-MakeTask1
 sudo scp -r ~/Skillfactory-MakeTask1/www.chiark.greenend.org.uk/~sgtatham/* $user:~/Skillfactory-MakeTask1/htmlForBugs/
-sudo scp ~/Skillfactory-MakeTask1/nginxDoc.conf $user:~/Skillfactory-MakeTask1/nginxDoc.conf
+sudo scp ~/Skillfactory-MakeTask1/nginx.conf $user:~/Skillfactory-MakeTask1/nginx.conf
 ssh $user sudo docker load -i ~/Skillfactory-MakeTask1/nginxImage.tar
-ssh $user sudo docker run --name test3 -d -p 8080:80 -v ~/Skillfactory-MakeTask1/htmlForBugs://usr/share/nginx/html:ro -v ~/Skillfactory-MakeTask1/nginxDoc.conf://etc/nginx/nginx.conf:ro nginx
+ssh $user sudo docker run --name test3 -d -p 8080:80 -v ~/Skillfactory-MakeTask1/htmlForBugs://usr/share/nginx/html:ro -v ~/Skillfactory-MakeTask1/nginx.conf://etc/nginx/nginx.conf:ro nginx
