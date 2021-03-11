7b6521d61200e388e6669f6c801b4df1a970319e
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-03-11 17:36:59 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,11 +7,5 @@ sudo wget --mirror -p --convert-links -P ~/Skillfactory-MakeTask1 https://www.ch
 ssh $user sudo rm ~/Skillfactory-MakeTask1/htmlForBugs/*
 #передаем новые данные сайта на локальный сервер
 sudo scp -r ~/Skillfactory-MakeTask1/www.chiark.greenend.org.uk/~sgtatham/* $user:~/Skillfactory-MakeTask1/htmlForBugs/
-#останавливаем запущенный контейнер
-ssh $user sudo docker stop test3
-#удаляем контейнер
-ssh $user sudo docker rm test3
-#запускаем новый контейнер с новыми данными сайта
-ssh $user sudo docker run --name test3 -d -p 8080:80 -v ~/Skillfactory-MakeTask1/htmlForBugs://usr/share/nginx/html:ro -v ~/Skillfactory-MakeTask1/nginx.conf://etc/nginx/nginx.conf:ro nginx
 #удаляем зеркало сайта на первой машине
 sudo rm -fr ~/Skillfactory-MakeTask1/www.chiark.greenend.org.uk/~sgtatham
